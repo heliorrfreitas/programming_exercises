7caf0ee8775f6cb1af68fcca42d4b551480fcf84
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-05-29 23:02:26 -0500

    Add hello_world.c file

---
+++ hello_world.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+To compile use the following command:
+
+cc -std=c99 -Wall hello_world.c -o hello_world
+
+cc is the compiler
+
+-std= lets the standar be specified
+
+c99 is the standard used to compile the c code
+
+-Wall it displays almost all warning of gcc
+
+-o means to place the output into a file
+
+*/
+
+int main(){
+	
+	printf("Hello world!\n");
+	
+	return 0;
+}
